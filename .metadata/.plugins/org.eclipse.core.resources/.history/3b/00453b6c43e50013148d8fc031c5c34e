d384d83e7ae446f3c906d74e3045deca33156596
Brian Munoz <s.munoz.brian@gmail.com>
2014-05-27 22:02:23 -0400

    add a preliminary display account view that display the input account name and balance

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3b/00453b6c43e50013148d8fc031c5c34e
@@ -0,0 +1,19 @@
+package models;
+
+public class AccountModel {
+	private double currentBalance;
+	
+	public AccountModel(double startingAmount){
+		currentBalance = startingAmount;
+		
+	}
+	
+	public void setCurrentBalance(double newAmount){
+		currentBalance = newAmount;
+	}
+	
+	public double getCurrentBalance(){
+		return currentBalance;
+	}
+	
+}
--- BudgetTracker/gen/com/example/budgettracker/R.java
+++ BudgetTracker/gen/com/example/budgettracker/R.java
@@ -1093,7 +1093,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1103,6 +1103,8 @@ containing a value of this type.
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int display_balance=0x7f050042;
+        public static final int display_name=0x7f050041;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
@@ -1198,14 +1200,17 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_create_new_account=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int fragment_create_new_account=0x7f03001a;
-        public static final int fragment_main=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_display_account=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int fragment_create_new_account=0x7f03001b;
+        public static final int fragment_display_account=0x7f03001c;
+        public static final int fragment_main=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
-        public static final int get_new_account=0x7f0c0000;
-        public static final int main=0x7f0c0001;
+        public static final int display_account=0x7f0c0000;
+        public static final int get_new_account=0x7f0c0001;
+        public static final int main=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1258,6 +1263,7 @@ containing a value of this type.
         public static final int save=0x7f0a0017;
         public static final int starting_balance=0x7f0a0016;
         public static final int title_activity_create_new_account=0x7f0a0012;
+        public static final int title_activity_display_account=0x7f0a0018;
         public static final int yes=0x7f0a0010;
     }
     public static final class style {
